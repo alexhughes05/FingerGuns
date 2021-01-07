75e20d3a6692ec6dd962810ccc4fc8e9d39da62b
Alex Hughes <ahughes.orlando@gmail.com>
2021-01-06 22:13:18 -0800

    Started Health System
    - added heart sprites
    - created Health script
    - must manually update health through inspector to see changes take affect.

---
+++ Finger Guns/Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int currentHealth;
+    public int numberOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (currentHealth > numberOfHearts)
+            currentHealth = numberOfHearts;
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth)
+                hearts[i].sprite = fullHeart;
+            else
+                hearts[i].sprite = emptyHeart;
+
+            if (i < numberOfHearts)
+                hearts[i].enabled = true;
+            else
+                hearts[i].enabled = false;
+        }
+    }
+
+    public void modifyHealth(int amount)
+    {
+        currentHealth += amount;
+    }
+}
