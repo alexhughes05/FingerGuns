09b1e95fdf3c87ea008def1ab27771de68a3b21a
Alex Hughes <ahughes.orlando@gmail.com>
2021-01-14 23:50:39 -0800

    Created Sandbox Scene with test map

--- Finger Guns/Assets/Scripts/Camera/CameraController.cs
+++ Finger Guns/Assets/Scripts/Camera/CameraController.cs
@@ -1,20 +1,37 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    #region Variables
-    public Transform followTarget;
-    public float smoothSpeed = 0.2f;
-    public Vector3 offset;
-    #endregion
 
-    #region Monobehaviour Callbacks
-    void FixedUpdate()
-    {
-        Vector3 targetPosition = followTarget.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, 
-            smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
-    }
-    #endregion
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start()
+	{
+		targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+
+			interpVelocity = targetDirection.magnitude * 5f;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+			transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+		}
+	}
 }
---
+++ Finger Guns/Assets/Scripts/Misc/PlatformCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCollision : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If player is moving up, ignore collisions between player and platforms
+        if (myRigidBody.velocity.y > 0)
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, true);
+        }
+        //else the collision will not be ignored
+        else
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, false);
+        }
+    }
+}
