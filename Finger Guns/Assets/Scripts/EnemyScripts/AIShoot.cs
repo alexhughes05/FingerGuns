b80b17ed6948770063ab4aedde70adc9bcebac55
d4des1gner <76670725+d4des1gner@users.noreply.github.com>
2021-01-14 19:42:03 -0500

    Basic Enemy AI
    Enemy can patrol across any platform.
    Enemy also shoots at an object with the "Player" tag when in range.

---
+++ Finger Guns/Assets/Scripts/EnemyScripts/AIShoot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShoot : MonoBehaviour
+{
+    #region Variables
+    //Components
+    private CircleCollider2D detectionCollider;
+
+    //Public
+    public Transform firePoint;
+    public GameObject enemyProjectile;
+    public float timeBtwShots;
+
+    //Private
+    private float currentTimeBtwShots;
+    #endregion
+
+    #region Monobehaviour Callbacks
+    void Start()
+    {
+        detectionCollider = GetComponent<CircleCollider2D>();
+
+        currentTimeBtwShots = timeBtwShots;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {        
+        if(collision.gameObject.tag == "Player")
+        {
+            firePoint.Rotate(collision.transform.position);
+            Shoot();
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    void Shoot()
+    {
+        if(currentTimeBtwShots <= 0)
+        {
+            Instantiate(enemyProjectile, firePoint.transform.position, Quaternion.identity);
+            currentTimeBtwShots = timeBtwShots;
+        }
+        else
+        {
+            currentTimeBtwShots -= Time.deltaTime;
+        }
+    }
+    #endregion
+}
---
+++ Finger Guns/Assets/Scripts/EnemyScripts/EnemyProjectile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    #region Variables
+    //Public
+    public float speed;
+
+    //Private
+    private Transform player;
+    private Vector2 target;
+    #endregion
+
+    #region Monobehaviour Callbacks
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+
+        //Point towards target position
+        Vector3 relativePos = player.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        rotation.x = transform.rotation.x;
+        rotation.y = transform.rotation.y;
+        transform.rotation = rotation;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag != "Enemy")
+            Destroy(gameObject);
+    }
+    #endregion
+}
