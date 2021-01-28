e8e4c240c0794a67fbcd64a61546e5afa3133d01
kyleralford <alfordkyler@gmail.com>
2021-01-28 16:21:40 -0500

    Sandbox Art Implementation
    Created new scene "Sandbox Art Implementation", which updates the sandbox with all of the art assets that have been created thus far.
    Also created scripts that auto-generate 2D box colliders on Awake() so that platforms can easily be resized without needing to change multiple values.

---
+++ Finger Guns/Assets/Art/Set Pieces/PlatformColliderCreation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformColliderCreation : MonoBehaviour
+{
+    public float heightPadding = -0.1f;
+    public float platformThickness = 0.1f;
+
+    private int targetLayer;
+    private float targetWidth;
+    private float targetYPos;
+
+    // Components
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    
+    private void Awake()
+    {
+        // Set gameObject's layer to "Platform"
+        targetLayer = LayerMask.NameToLayer("Platform");
+        if (targetLayer < 0)
+        {
+            Debug.Log("Platforms are trying to assign themselves to the \"Platform\" layer for platforming logic, but no such layer exists.");
+        }
+        else if (gameObject.layer != targetLayer)
+        {
+            gameObject.layer = targetLayer;
+        }
+
+        // Set up component dependencies
+        if(GetComponent<SpriteRenderer>() == null)
+        {
+            spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        }
+        else
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (GetComponent<BoxCollider2D>() == null)
+        {
+            boxCollider = gameObject.AddComponent<BoxCollider2D>();
+        }
+        else
+        {
+            boxCollider = GetComponent<BoxCollider2D>();
+        }
+
+        // Set variables to be used in manipulating the BoxCollider2D
+        targetWidth = spriteRenderer.size.x;
+        targetYPos = (spriteRenderer.size.y / 2);
+        targetYPos -= (platformThickness / 2);
+        targetYPos += heightPadding;
+
+        // Set BoxCollider2D values to match the SpriteRenderer
+        boxCollider.size = new Vector2(targetWidth, platformThickness);
+        boxCollider.offset = new Vector2(0.0f, targetYPos);
+    }
+}
