55d8b2f654315b914e53ebc785bc858c38df047c
d4des1gner <76670725+d4des1gner@users.noreply.github.com>
2021-02-24 21:16:02 -0500

    Stationary Blade Fix
    Fixed stationary blade damage issue by deleting "Projectile" script off of the stationary blade prefab

--- Finger Guns/Assets/Scripts/Obstacles/Projectile.cs
+++ Finger Guns/Assets/Scripts/Obstacles/Projectile.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-{
+{/*
     public int damage;
     bool tookHit = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var damageDealer = collision.collider.gameObject;
-        if (damageDealer.CompareTag("Player"))
+        if (damageDealer.layer == 10)
         {
             if (damageDealer && tookHit == false)
             {
-                damageDealer.GetComponentInParent<PlayerHealth>().ModifyHealth(-damage);
+                
                 tookHit = true;
             }
         }
-    }
+    }*/
 }
