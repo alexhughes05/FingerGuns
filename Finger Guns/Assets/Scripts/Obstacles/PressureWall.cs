2dbe610a0003fb5254a63cc29c1f884c2c9af2eb
Alex Hughes <ahughes.orlando@gmail.com>
2021-06-09 18:38:19 -0800

    Revert "Pressure Wall Update"
    This reverts commit e6c0c8300ff294ae22242d1657fe39f9b93c33fc.

--- Finger Guns/Assets/Scripts/Obstacles/PressureWall.cs
+++ Finger Guns/Assets/Scripts/Obstacles/PressureWall.cs
@@ -45,14 +45,14 @@ public class PressureWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3(cam.transform.position.x - initialDistanceBehindPlayer, cam.transform.position.y, 0);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, cam.transform.position.y, 0);
         size += (speedOfWall * Time.deltaTime); //The wall is expanded based on the speed. A higher speed will expand it faster
         offset = size * 0.5f;  //In order to keep the collider bounded on the left, the offset has to be half the amount of the size. This will allow it to only expand in the right direction.
         col.offset = new Vector2(offset, col.offset.y); //set the offset of the collider
         col.size = new Vector2(size, col.size.y);  //set the size of the collider
         //particle effect
         shape.position = new Vector2(offset * 2f, shape.position.y);  //To match the rate of the wall, the particle effect must expand at a rate of 2 * the collider offset
-
+        
         if (blackGradient != null)
         {
             blackGradient.position = new Vector3(shape.position.x + transform.position.x - 7, cam.transform.position.y, 0);
