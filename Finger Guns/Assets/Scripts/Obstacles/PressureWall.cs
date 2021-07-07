d61edc60c13873557b6566171de30c396cd1d8b8
Alex Hughes <ahughes.orlando@gmail.com>
2021-07-07 01:06:34 -0800

    Pressure Wall Changes
    Pressure wall now transforms position instead of expanding. Same goes for black fill. Also added a player stopper which instantly kills the player to ensure they don't go through the other side of the wall. Created a prefab of the pressure wall and made the dissolve shader, player stopper, and black fill its children. Overall the code is greatly simplified.

--- Finger Guns/Assets/Scripts/Obstacles/PressureWall.cs
+++ Finger Guns/Assets/Scripts/Obstacles/PressureWall.cs
@@ -5,61 +5,26 @@ using UnityEngine;
 public class PressureWall : MonoBehaviour
 {
     //public variables
-    [Range(0, 100)]
-    [SerializeField] int initialDistanceBehindPlayer;
     [SerializeField] float speedOfWall;
     [SerializeField] float timeBtwDamageTicks;
-    [SerializeField] Transform dissolveShader;
 
     //private variables
-    private Camera cam;
-    private Transform blackFill;
     private BoxCollider2D col;
     private PlayerHealth playerHealth;
-    private float offset;
-    private float size;
     private Coroutine co;
     private bool playerDead;
     private bool coroutineStarted;
-    private ParticleSystem.ShapeModule shape;
 
-    private void Awake()
+    private void Awake() 
     {
-        cam = Camera.main;
         col = GetComponent<BoxCollider2D>();
         playerHealth = FindObjectOfType<PlayerHealth>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.transform.position = new Vector2(cam.transform.position.x - initialDistanceBehindPlayer, cam.transform.position.y);
-        //shape = ps.shape;  //Alows you to modify the boundaries of the particle system
-        blackFill = GameObject.Find("Black Fill Container").transform;
-        blackFill.position = new Vector2(transform.position.x + size - 0.5f, cam.transform.position.y + 5);
-    }
-
-    public float GetPressureWallXPos()
-    {
-        return (transform.position.x + size); //The x position of the pressure wall is the current position + the size (however much it has expanded)
-    }
+    } 
 
     // Update is called once per frame
-    void Update()
-    {
-        size += (speedOfWall * Time.deltaTime); //The wall is expanded based on the speed. A higher speed will expand it faster
-        offset = size * 0.5f;  //In order to keep the collider bounded on the left, the offset has to be half the amount of the size. This will allow it to only expand in the right direction.
-        col.offset = new Vector2(offset, col.offset.y); //set the offset of the collider
-        col.size = new Vector2(size, col.size.y);  //set the size of the collider
-        //particle effect
-        dissolveShader.position = new Vector2(transform.position.x + size, cam.transform.position.y);  //To match the rate of the wall, the particle effect must expand at a rate of 2 * the collider offset
+    void Update() => transform.position += Vector3.right * Time.deltaTime * speedOfWall * 2;
 
-        if (blackFill != null)
-        {
-            blackFill.localScale = new Vector3(offset * 33f, blackFill.localScale.y, blackFill.localScale.z);
-        }
-    }
-
+    public float GetPressureWallXPos() => transform.position.x + col.size.x + 20; //The x position of the pressure wall
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !playerDead && !coroutineStarted)
